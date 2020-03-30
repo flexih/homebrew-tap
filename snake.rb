17f5a1ddb3dd924929322fb699dc883587b70a34
Xinghua <flexih@gmail.com>
2020-03-30 17:47:44 +0800

    Update snake.rb

--- snake.rb
+++ snake.rb
@@ -5,7 +5,7 @@
 class Snake < Formula
   desc "Yet Another Mach-O Unused ObjC Selector/Class/Protocol Detector. 检测ObjC无用方法、无用类、无用协议。"
   homepage "https://github.com/flexih/Snake"
-  url "https://github.com/flexih/Snake/releases/download/1.3/snake.zip"
+  url "https://github.com/flexih/Snake/releases/download/v1.3/snake.zip"
   version "1.3"
   sha256 "9f6ce017bb2041d95e6522efbcad36bc468ac860d1b59c18ff52d2ea44fa1026"
 
