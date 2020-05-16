ee44b6bf025c12ae18182326311b1c079c60e675
Xinghua <flexih@gmail.com>
2020-05-16 22:51:30 +0800

    v1.5

--- snake.rb
+++ snake.rb
@@ -5,9 +5,9 @@
 class Snake < Formula
   desc "Yet Another Mach-O Unused ObjC Selector/Class/Protocol Detector. 检测ObjC无用方法、无用类、无用协议。"
   homepage "https://github.com/flexih/Snake"
-  url "https://github.com/flexih/Snake/releases/download/v1.4/snake.zip"
-  version "1.4"
-  sha256 "de383dda20659f2089855dac0ae8fb50896616aaada5a899047b4e966d535411"
+  url "https://github.com/flexih/Snake/releases/download/v1.5/snake.zip"
+  version "1.5"
+  sha256 "49b27ea37006b7f0ce12ed0e3c9228e16715aed282e254a8f7c00ff8c9e10ab4"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
